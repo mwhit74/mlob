ee505fcb92d25c7b7dbf35450668521b3ae11b40
mwhit74 <mwhit74@gmail.com>
2017-03-12 15:28:41 -0500

    Shear calc needs to verified but it appears to work

--- moving_load.py
+++ moving_load.py
@@ -22,9 +22,15 @@ def main(num_axles, axle_num, axle_spacing, axle_wt, span_length, num_nodes):
             cur_axle_loc = move_axle_loc(x, axle_spacing, abs_axle_spacing, axle_id, prev_axle_loc, num_axles)
             #print "cur_axle_loc: " + str(cur_axle_loc)
             Pt, xt = total_load_and_loc(cur_axle_loc, axle_wt, span_length, num_axles)
+            Pl, xl = load_to_left_and_loc(cur_axle_loc, axle_wt, x, span_length, num_axles)
             Pr, xr = load_to_right_and_loc(cur_axle_loc, axle_wt, x, span_length, num_axles)
-             
-            Vc = Pt*(xt/span_length) - Pr
+
+
+            Ra = Pt*((span_length-xt)/span_length)
+            Rb = Pt*(xt/span_length)
+            
+            Va =  
+            Vb = Ra - Pl
             print "Vc: " + str(Vc)
             if Vc < 0:
                 if Vc < minV:
@@ -98,6 +104,26 @@ def total_load_and_loc(cur_axle_loc, axle_wt, span_length, num_axles):
 
     return Pt, xt
     
+def load_to_left_and_loc(cur_axle_loc, axle_wts, x, span_length, num_axles):
+    Pl = 0.0
+    xl = 0.0
+    sum_Px = 0.0
+    
+    for i in range(num_axles):
+        if cur_axle_loc[i] >= 0.0 and cur_axle_loc[i] <= x:
+            Pl = Pl + axle_wts[i]
+            sum_Px = sum_Px + cur_axle_loc[i]*axle_wts[i]
+    
+    if Pl == 0:
+        xl = 0
+    else:        
+        xl = sum_Px/Pl
+
+    print "Pl: " + str(Pl)
+    print "xl: " + str(xl)
+    
+    return Pl, xl
+
 def load_to_right_and_loc(cur_axle_loc, axle_wts, x, span_length, num_axles):
     Pr = 0.0
     xr = 0.0
@@ -132,8 +158,8 @@ if __name__ == "__main__":
     #input
     #axle_spacing = [8.00, 13.00, 18.00, 23.00, 32.00, 37.00, 43.00, 48.00, 56.00, 64.00, 69.00, 74.00, 79.00, 88.00, 93.00, 99.00, 104.00]
     #axle_wt = [40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00, 40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00]
-    axle_spacing = [3.00, 2.00]
-    axle_wt = [13.0, 10.0, 5.00]
+    axle_spacing = []
+    axle_wt = [1.0]
     num_axles = len(axle_wt)
     axle_num = get_axle_num(num_axles)
     span_length = 20.0
