3c0d38f11ff77fe3e77440e74bc798a703500429
Emma Yaffe <yaffee@flip1.engr.oregonstate.edu>
2017-03-20 18:41:28 -0700

    added function to get inputs

--- mlob.py
+++ mlob.py
@@ -351,8 +351,65 @@ def manager(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     output(V_max1, V_min1, M_max1, V_max2, V_min2, M_max2, Rmax_pier,
         analysis_time)
 
+def get_inputs():
+    """Get user inputs for calculation values"""
+    print "Enter axle spacing values separated by a space. Hit enter when done."
+    while True:
+        try:
+            axle_spacing=([float(x) for x in raw_input().split(' ')])
+        except ValueError:
+            print "Invalid values. Please only enter numbers.\nStart from beginning of axle spacing values:"
+        else:
+            break
 
-if __name__ == "__main__":
+    print "Enter axle weight values separated by a space. Hit enter when done."
+    while True:
+        try:
+            axle_wt=([float(x) for x in raw_input().split(' ')])
+            if len(axle_wt) != (len(axle_spacing) + 1):
+                print "Please enter %d axle weight values:" % (len(axle_spacing) + 1)
+                continue
+        except ValueError:
+            print "Invalid values. Please only enter numbers.\nStart from beginning of axle weight values:"
+        else:
+            break
+
+    print "Enter value of span length 1. Hit enter when done."
+    while True:
+        try:
+            span_length1=(float(raw_input()))
+        except ValueError:
+            print "Invalid value. Please enter one number."
+        else:
+            break
+
+    print "Enter value of span length 2. Hit enter when done."
+    while True:
+        try:
+            span_length2=(float(raw_input()))
+        except ValueError:
+            print "Invalid value. Please enter one number."
+        else:
+            break
+
+    print "Enter the number of nodes as an integer. Number should be odd and greater than or equal to 3."
+    while True:
+        try:
+            num_nodes=(int(raw_input()))
+            if num_nodes < 3:
+                print "Number of nodes should be greater than or equal to 3\nPlease enter the number of nodes:"
+                continue
+            elif (num_nodes % 2) == 0:
+                print "Please enter an odd number of ndoes:"
+                continue
+        except ValueError:
+            print "Invalid value. Please enter an integer:"
+        else:
+            break
+
+    return axle_spacing, axle_wt, span_length1, span_length2, num_nodes
+
+ __name__ == "__main__":
     #input
     axle_spacing = [8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00, 8.00, 8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00]
     axle_wt = [40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00, 40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00]
