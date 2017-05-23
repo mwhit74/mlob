1b3b80afa34ca901791547cb4968521d6766bdcb
mwhit74 <mwhit74@gmail.com>
2017-05-23 10:06:42 -0500

    Started testing add_trailing_load.
    Wrote three basic tests for add trailing load function. A two span with
    trailing load, a single span with trailing load, and a single span without
    trailing load.
    
    I also still need to check the exceptions being raised.

--- test/test_mlob.py
+++ test/test_mlob.py
@@ -25,8 +25,14 @@ class TestUserInput(unittest.TestCase):
 class TestMlob(unittest.TestCase):
 
     def setUp(self):
-        self.axle_spacing_E80 = []
-        self.axle_wt_E80 = []
+        self.axle_spacing_E80 = [8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00,
+                                 5.00, 8.00, 8.00, 5.00, 5.00, 5.00, 9.00,
+                                 5.00, 6.00, 5.00]
+        self.axle_wt_E80 = [40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00,
+                            52.00, 52.00, 40.00, 80.00, 80.00, 80.00, 80.00,
+                            52.00, 52.00, 52.00, 52.00]
+        self.axle_spacing_286k = [3.042,5.83,21.5,5.83,3.042]
+        self.axle_wt_286k = [71.50,71.50,71.50,71.50]
         #ul = unit load
         self.axle_spacing_ul = [0.0]
         self.axle_wt_ul = [1.0]
@@ -41,9 +47,11 @@ class TestMlob(unittest.TestCase):
         self.num_user_nodes_50 = 50
 
         self.space_to_trailing_load_E80 = 5.0
+        self.space_to_trailing_load_286k = 0.0
         self.space_to_trailing_load_ul = 0.0
 
         self.distributed_load_E80 = 8.0
+        self.distributed_load_286k = 0.0
         self.distributed_load_ul = 0.0
 
     def tearDown(self):
@@ -110,3 +118,106 @@ class TestMlob(unittest.TestCase):
     def test_span_begin_end_coords_l1pos_l2neg(self):
         self.assertRaises(ValueError, mlob.span_begin_end_coords,
                 self.span_50, -1*self.span_25)
+
+    def test_add_trailing_load_single_span_e80(self):
+        span1_begin = 0.0
+        span2_end = 20.0
+        mlob.add_trailing_load(self.axle_spacing_E80,
+                          self.axle_wt_E80,
+                          self.space_to_trailing_load_E80,
+                          self.distributed_load_E80,
+                          span1_begin,
+                          span2_end)
+
+        caxle_spacing = [8.0,5.0,5.0,5.0,9.0,5.0,6.0,5.0,8.0,
+                         8.0,5.0,5.0,5.0,9.0,5.0,6.0,5.0,5.0,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
+
+        caxle_wt = [40.0,80.0,80.0,80.0,80.0,52.0,52.0,52.0,52.0,
+                    40.0,80.0,80.0,80.0,80.0,52.0,52.0,52.0,52.0, 2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5]
+
+        for cas, caw, uas, uaw in zip(caxle_spacing, 
+                                      caxle_wt,
+                                      self.axle_spacing_E80,
+                                      self.axle_wt_E80):
+            self.assertAlmostEqual(cas, uas, places=3)
+            self.assertAlmostEqual(cas, uas, places=3)
+
+    def test_add_trailing_load_two_span_e80(self):
+        span1_begin = 0.0
+        span2_end = 70.0
+        mlob.add_trailing_load(self.axle_spacing_E80,
+                          self.axle_wt_E80,
+                          self.space_to_trailing_load_E80,
+                          self.distributed_load_E80,
+                          span1_begin,
+                          span2_end)
+
+        caxle_spacing = [8.0,5.0,5.0,5.0,9.0,5.0,6.0,5.0,8.0,
+                         8.0,5.0,5.0,5.0,9.0,5.0,6.0,5.0,5.0,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+                         0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
+
+        caxle_wt = [40.0,80.0,80.0,80.0,80.0,52.0,52.0,52.0,52.0,
+                    40.0,80.0,80.0,80.0,80.0,52.0,52.0,52.0,52.0, 2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,
+                    2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5]
+
+        for cas, caw, uas, uaw in zip(caxle_spacing, 
+                                      caxle_wt,
+                                      self.axle_spacing_E80,
+                                      self.axle_wt_E80):
+            self.assertAlmostEqual(cas, uas, places=3)
+            self.assertAlmostEqual(cas, uas, places=3)
+
+    def test_add_trailing_load_single_span_none(self):
+        span1_begin = 0.0
+        span2_end = 20.0
+        mlob.add_trailing_load(self.axle_spacing_286k,
+                          self.axle_wt_286k,
+                          self.space_to_trailing_load_286k,
+                          self.distributed_load_286k,
+                          span1_begin,
+                          span2_end)
+
+        caxle_spacing = [3.042,5.83,21.50,5.83,3.042]
+
+        caxle_wt = [71.50,71.50,71.50,71.50]
+
+        for cas, caw, uas, uaw in zip(caxle_spacing, 
+                                      caxle_wt,
+                                      self.axle_spacing_286k,
+                                      self.axle_wt_286k):
+            self.assertAlmostEqual(cas, uas, places=3)
+            self.assertAlmostEqual(cas, uas, places=3)
