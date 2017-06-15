4f96e6a9a007b39725a764385790535fcafbaf67
mwhit74 <mwhit74@gmail.com>
2017-06-14 19:45:04 -0500

    Removed unit labels from output tables. Specified span for max reaction.
    The program is independent of units and it is up to the user to keep track of
    the units and be consisent. Therefore, I removed the unit labels from the output
    tables.
    
    I noticed that the Vmax and Mmax output doesn't make sense without knowing which
    span it corresponds to and it is not obvious which span it does correspond to.
    Clarified which span each maximum corresponds to.

--- ml/output.py
+++ ml/output.py
@@ -21,9 +21,9 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     out_tb += "\n\nOUTPUT\n"
     out_tb += "SPAN 1"
     out_tb += "\n" #span 1 title spacing
-    out_tb += "{0:^15s}{1:^15s}{2:^15s}".format("x [ft]",
-                                                "Vmax [kip]",
-                                                "Mmax [kip-ft]")
+    out_tb += "{0:^15s}{1:^15s}{2:^15s}".format("x",
+                                                "Vmax",
+                                                "Mmax")
     out_tb += "\n" #span 1 header spacing
   
     for x,Vmax,Mmax in zip(node_loc, V_max1, M_max1):
@@ -34,7 +34,7 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     out_tb += "\n" #span 1 table spacing
   
   
-    out_val += """Vmax [kip]: {0:<-.3f}\nMmax [kip-ft]: {1:<-.3f}""".format(max(V_max1)/2,
+    out_val += """Span 1 Vmax: {0:<-.3f}\n Span 1 Mmax: {1:<-.3f}""".format(max(V_max1)/2,
                                                                             max(M_max1)/2)
     out_val += "\n" #span 1 max/min spacing
   
@@ -42,9 +42,9 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     if V_max2 != []:
         out_tb += "SPAN 2"
         out_tb += "\n" #span 2 title spacing
-        out_tb += "{0:^15s}{1:^15s}{2:^15s}".format("x [ft]",
-                                                    "Vmax [kip]",
-                                                    "Mmax [kip-ft]")
+        out_tb += "{0:^15s}{1:^15s}{2:^15s}".format("x",
+                                                    "Vmax",
+                                                    "Mmax")
         out_tb += "\n" #span 2 header spacing
   
         for x,Vmax,Mmax in zip(node_loc, V_max2, M_max2):
@@ -54,10 +54,10 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
   
         out_tb += "\n\n" #span 2 table spacing
         
-        out_val += """Vmax [kip]: {0:<-.3f}\nMmax [kip-ft]: {1:<-.3f}""".format(max(V_max2)/2,
+        out_val += """Span 2 Vmax: {0:<-.3f}\nSpan 2 Mmax: {1:<-.3f}""".format(max(V_max2)/2,
                                                                                 max(M_max2)/2)
   
-        out_val += "\nRmax_pier: {0:<-.3f}".format(Rmax_pier/2)
+        out_val += "\nRmax pier or floorbeam: {0:<-.3f}".format(Rmax_pier/2)
       
     print echo_input + out_tb + out_val
        
