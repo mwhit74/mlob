35983480de2110607d72d2863e1129cbb44465c9
mwhit74 <mwhit74@gmail.com>
2017-06-14 19:55:47 -0500

    Updated output formatting.

--- ml/output.py
+++ ml/output.py
@@ -8,7 +8,7 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     out_tb = ""
     out_val = ""
   
-    echo_input += "\n\nECHO INPUT\n"
+    echo_input += "\n\n\n\n---ECHO USER INPUT---\n"
     echo_input += "Axle spacing: " + str(axle_spacing) + "\n"
     echo_input += "Axle weights: " + str(axle_wt) + "\n"
     echo_input += "Length Span 1: " + str(span_length1) + "\n"
@@ -18,7 +18,7 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     echo_input += "Distributed trailing load: " + str(distributed_load) + "\n"
   
   
-    out_tb += "\n\nOUTPUT\n"
+    out_tb += "\n\n---PROGRAM OUTPUT---\n"
     out_tb += "SPAN 1"
     out_tb += "\n" #span 1 title spacing
     out_tb += "{0:^15s}{1:^15s}{2:^15s}".format("x",
@@ -34,9 +34,8 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
     out_tb += "\n" #span 1 table spacing
   
   
-    out_val += """Span 1 Vmax: {0:<-.3f}\n Span 1 Mmax: {1:<-.3f}""".format(max(V_max1)/2,
+    out_val += """Span 1 Vmax: {0:<-.3f}\nSpan 1 Mmax: {1:<-.3f}""".format(max(V_max1)/2,
                                                                             max(M_max1)/2)
-    out_val += "\n" #span 1 max/min spacing
   
   
     if V_max2 != []:
@@ -54,13 +53,13 @@ def write_output(axle_spacing, axle_wt, span_length1, span_length2, num_nodes,
   
         out_tb += "\n\n" #span 2 table spacing
         
-        out_val += """Span 2 Vmax: {0:<-.3f}\nSpan 2 Mmax: {1:<-.3f}""".format(max(V_max2)/2,
+        out_val += """\nSpan 2 Vmax: {0:<-.3f}\nSpan 2 Mmax: {1:<-.3f}""".format(max(V_max2)/2,
                                                                                 max(M_max2)/2)
   
         out_val += "\nRmax pier or floorbeam: {0:<-.3f}".format(Rmax_pier/2)
       
     print echo_input + out_tb + out_val
        
-    print "Runtime [sec]: {0:<.3f}\n\n".format(analysis_time)
+    print "\nRuntime [sec]: {0:<.3f}\n\n".format(analysis_time)
 
 
