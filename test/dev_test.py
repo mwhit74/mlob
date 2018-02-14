e38b067804503a43f1b7ab9cca1c3604bb579f02
MLW <mwhit74@gmail.com>
2018-02-14 13:54:00 -0600

    Added functionality to run multiple axle configs and multiple span lengths.
    Added a couple of for loops to be able to run multiple axle configurations and
    multiple span lengths/configurations.
    
    I want to add an input file and provide this ability to users instead of it just
    being in the dev_test file that I use.

--- test/dev_test.py
+++ test/dev_test.py
@@ -6,6 +6,12 @@ import timeit
 import pdb
 import matplotlib.pyplot as plt
 
+"""
+To run dev_test.py
+1. Navigate to moving_loads directory
+2. python -m test.dev_test
+"""
+
 def manager():
     """Controls the global program execution.
 
@@ -17,60 +23,83 @@ def manager():
     """
      
     #testing input
-    axle_spacing = [8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00, 8.00, 8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00]
-    axle_wt = [40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00, 40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00]
-    sf = 72.0/80.0 #scale factor
-    axle_wt = [round(sf*p,3) for p in axle_wt]
-    space_to_trailing_load = 5.00
-    distributed_load = 8.00
-    distributed_load = round(sf*distributed_load,3)
-    #axle_spacing = []
-    #axle_wt = [1.0]
-    #space_to_trailing_load = 0.0
-    #distributed_load = 0.0
-    span_length1 = 26.75
-    span_length2 = 0.0
+    axle_spacing1 = [8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00, 8.00, 8.00, 5.00, 5.00, 5.00, 9.00, 5.00, 6.00, 5.00]
+    axle_wt1 = [40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00, 40.00, 80.00, 80.00, 80.00, 80.00, 52.00, 52.00, 52.00, 52.00]
+    sf1 = 80.0/80.0 #scale factor
+    axle_wt1 = [round(sf1*p,3) for p in axle_wt1]
+    space_to_trailing_load1 = 5.00
+    distributed_load1 = 8.00
+    distributed_load1 = round(sf1*distributed_load1,3)
+
+    axle_spacing2 = [5.0, 6.0,5.0]
+    axle_wt2 = [100.0, 100.0, 100.0, 100.0]
+    space_to_trailing_load2 = 0.0
+    distributed_load2 = 0.0
+
+
+    span_length1_1 = 30.083
+    span_length2_1 = 0.0
+
+    #span_length1_2 = 0.0
+    #span_length2_2 = 0.0
+
     #num_nodes should always be odd to place a node at midspan and at 
     #each support
     #a minimum of 21 nodes should be used for analysis
     num_nodes = 21
+
+    asps = [axle_spacing1, axle_spacing2]
+    awts = [axle_wt1, axle_wt2]
+    sttls = [space_to_trailing_load1, space_to_trailing_load2]
+    dls = [distributed_load1, distributed_load2]
+
+    sls = [[span_length1_1, span_length2_1]]
      
     #(axle_spacing, axle_wt, space_to_trailing_load, distributed_load, 
     #span_length1, span_length2, num_nodes) = get_input()
 
-    #for echoing user input in the output
-    uias = [] #user_input_axle_spacing
-    [uias.append(x) for x in axle_spacing]
-    uiaw = [] #user_input_axle_wt
-    [uiaw.append(x) for x in axle_wt]
+    for asp, awt, sttl, dl in zip(asps, awts, sttls, dls):
+        for sl in sls:
+            axle_spacing = asp
+            axle_wt = awt
+            space_to_trailing_load = sttl
+            distributed_load = dl
+            span_length1 = sl[0]
+            span_length2 = sl[1]
 
-    start = timeit.default_timer()
-    #pdb.set_trace()
-    (node_loc, 
-     V_max1, M_corr1, V_max1_axle,
-     M_max1, V_corr1, M_max1_axle,
-     V_max2, M_corr2, V_max2_axle,
-     M_max2, V_corr2, M_max2_axle,
-     Rmax_pier, Rmax_pier_axle,
-    span1_begin, span2_begin) = analyze_vehicle(axle_spacing, axle_wt,
-                                               span_length1, span_length2,
-                                               num_nodes,
-                                               space_to_trailing_load, 
-                                               distributed_load)
-    stop = timeit.default_timer()
+            #for echoing user input in the output
+            uias = [] #user_input_axle_spacing
+            [uias.append(x) for x in axle_spacing]
+            uiaw = [] #user_input_axle_wt
+            [uiaw.append(x) for x in axle_wt]
 
-    analysis_time = stop - start
+            start = timeit.default_timer()
+            #pdb.set_trace()
+            (node_loc, 
+             V_max1, M_corr1, V_max1_axle,
+             M_max1, V_corr1, M_max1_axle,
+             V_max2, M_corr2, V_max2_axle,
+             M_max2, V_corr2, M_max2_axle,
+             Rmax_pier, Rmax_pier_axle,
+            span1_begin, span2_begin) = analyze_vehicle(axle_spacing, axle_wt,
+                                                       span_length1, span_length2,
+                                                       num_nodes,
+                                                       space_to_trailing_load, 
+                                                       distributed_load)
+            stop = timeit.default_timer()
 
-    write_output(uias, uiaw, span_length1, span_length2, num_nodes,
-            space_to_trailing_load, distributed_load, node_loc, 
-            V_max1, M_corr1, V_max1_axle,
-            M_max1, V_corr1, M_max1_axle,
-            V_max2, M_corr2, V_max2_axle,
-            M_max2, V_corr2, M_max2_axle,
-            Rmax_pier, Rmax_pier_axle,
-            analysis_time, span1_begin, span2_begin)
+            analysis_time = stop - start
 
-    #graph(node_loc, V_max1, M_max1, V_max2, M_max2)
+            write_output(uias, uiaw, span_length1, span_length2, num_nodes,
+                    space_to_trailing_load, distributed_load, node_loc, 
+                    V_max1, M_corr1, V_max1_axle,
+                    M_max1, V_corr1, M_max1_axle,
+                    V_max2, M_corr2, V_max2_axle,
+                    M_max2, V_corr2, M_max2_axle,
+                    Rmax_pier, Rmax_pier_axle,
+                    analysis_time, span1_begin, span2_begin)
+
+            #graph(node_loc, V_max1, M_max1, V_max2, M_max2)
 
 def graph(node_loc, V_max1, M_max1, V_max2, M_max2):
     node_loc = node_loc[:(len(node_loc)/2+1)]
